4591e2e9cf199e77e8607ca54ca85ea39f7c5407
luckky <guodashun@163.com>
2020-08-01 08:40:41 +0800

    add time eval

--- eval.py
+++ eval.py
@@ -5,6 +5,7 @@ import mpl_toolkits.mplot3d
 import os
 from dataset import load_npz
 from traj_speed import cubic_speed
+import time
 # from sklearn.svm import SVR
 
 # npz_list = os.listdir('./npz_502/')
@@ -24,20 +25,14 @@ def eval_position():
     ax.set_zlabel('Z')  # 设置z坐标轴
     pos_raw_data = [0]*3
     pos_pre_data = [0]*3
+    t_start = time.time()
     for i in range(3): # x,y,z
-        # print('before load', pos_raw_data)s
         pos_raw_data[i] = load_npz("test_data", i, frame_rate)
         pos_pre_data[i] = pos_raw_data[i][1][0].copy()
         # print('after load what is pre',pos_raw_data[i][1], pos_pre_data[i])
         model = joblib.load(model_dir + model_list[i])
-        # print('fuck', pos_raw_data[i])
-        # print('fuck',np.array(pos_raw_data[i]).shape)
         speed_data = cubic_speed(pos_raw_data[i])
-        # print('fuck speed shape', np.array(speed_data).shape, np.array(speed_data[0]).shape)
-        # print('fuck speed', speed_data)
         test_data = np.array(speed_data[0])[time_start:time_start+time_step] # need test
-        # print('fuck test shape',test_data.shape)
-        # print(pos_raw_data[i][0].shape)
         for j in range(pos_raw_data[i][0].shape[1] - time_step-time_start):
             if verbose and j == 1:
                 print("raw speed", speed_data)
@@ -56,6 +51,8 @@ def eval_position():
                 print("b", test_data)
             pos_pre_data[i][time_step+j+time_start] = pre_x
     # print('before plot', pos_raw_data)
+    if verbose:
+        print("cost time:", time.time()-t_start)
     ax.plot(pos_raw_data[0][1][0],pos_raw_data[1][1][0],pos_raw_data[2][1][0], color='red')
     ax.plot(pos_pre_data[0],pos_pre_data[1],pos_pre_data[2], color='green')
     plt.show()
